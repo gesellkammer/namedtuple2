ceaa2a23dd8d61f3e99869d2c6ebea1a0163d0b3
Eduardo Moguillansky <eduardo.moguillansky@gmail.com>
2013-07-16 19:29:35 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='namedtuple2',
+      version='1.0',
+      description='A drop-in replacement for collections.namedtuple allowing default values',
+      author='Robert Handke',
+      author_email='robert.handke@hushmail.com',
+      url='https://github.com/gesellkammer/namedtuple2',
+      py_modules=['namedtuple2'],
+      # packages=['namedtuple2']
+     )
